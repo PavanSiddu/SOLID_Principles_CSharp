3acd19ea7876d34f0236afe860e891912495a219
Pavan Siddu <Pavan.Siddu@ct1limited.com>
2023-09-09 12:53:15 +0530

    first commit

---
+++ Dependency Inversion Principle Demo/DIP-Solution/ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In dependency Inversion no module should depend on other module.it should only depent on Abstrction(Interface)
+            //i.e, here in this example we are not creating any objects of other class.(new className() should not be present)
+
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Tim";
+            owner.LastName = "Corey";
+            owner.EmailAddress = "tim@iamtimcorey.com";
+            owner.PhoneNumber = "555-1212";
+            
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+           
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
